10e9301b0fd4bb843afaf906ce2f0f1759190d70
itsdragzz <kelvinliang123@icloud.com>
2025-09-30 10:23:53 +1000

    broken changes

--- RL-BM/src/App.jsx
+++ RL-BM/src/App.jsx
@@ -18,6 +18,21 @@ function App() {
           <Route path="/contact" element={<ContactUs />} />
         </Routes>
         <Footer />
+    <>
+
+      <div> 
+        <Header />
+      </div>
+
+
+
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
     </Router>
   );
