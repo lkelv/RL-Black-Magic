49a7c3fdf40eadb93924e28e2d587c660aa35b22
itsdragzz <kelvinliang123@icloud.com>
2025-09-30 12:32:29 +1000

    added activate page

--- RL-BM/src/App.jsx
+++ RL-BM/src/App.jsx
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Activate from './pages/Activate';
+import ActivateMethods from './pages/ActivateMethods';
+import ActivateSpecialist from './pages/ActivateSpecialist';
+import ActivateBoth from './pages/ActivateBoth';
 import InstallationGuide from './pages/InstallationGuide';
 import ContactUs from './pages/ContactUs';
 import './App.css';
@@ -18,6 +21,9 @@ function App() {
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/activate" element={<Activate />} />
+                        <Route path="/activate/methods" element={<ActivateMethods />} />
+                        <Route path="/activate/specialist" element={<ActivateSpecialist />} />
+                        <Route path="/activate/both" element={<ActivateBoth />} />
                         <Route path="/installation-guide" element={<InstallationGuide />} />
                         <Route path="/contact-us" element={<ContactUs />} />
                     </Routes>
