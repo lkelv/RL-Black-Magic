2eab4de17baae3938a6465f5a24fbd76cc4cb8e8
itsdragzz <kelvinliang123@icloud.com>
2025-09-30 12:01:08 +1000

    update location of pages
    Co-Authored-By: Yeeheng Lee <160732922+Pengu-8@users.noreply.github.com>

--- RL-BM/src/App.jsx
+++ RL-BM/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';
+import Home from './pages/Home';
 import Activate from './pages/Activate';
 import InstallationGuide from './pages/InstallationGuide';
 import ContactUs from './pages/ContactUs';
