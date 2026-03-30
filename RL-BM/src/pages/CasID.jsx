a359c97cb2bf0c0fb1b51c5ec7dae9e50c5519db
itsdragzz <kelvinliang123@icloud.com>
2026-03-30 19:08:35 +1100

    Removed number in header of casID

--- RL-BM/src/pages/CasID.jsx
+++ RL-BM/src/pages/CasID.jsx
@@ -64,7 +64,7 @@ function CasID() {
                 
                 <div className="bg-[#2d5047] rounded-2xl p-8 md:p-12">
                     <h2 className="text-2xl md:text-3xl font-bold mb-8 text-[#f4a52e] text-center">
-                        3. Enter the <u>last 6 characters</u> of the CAS ID
+                        Enter the <u>last 6 characters</u> of the CAS ID
                     </h2>
 
                     <div className="mb-6 space-y-8">
